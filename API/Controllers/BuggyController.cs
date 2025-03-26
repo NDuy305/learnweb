554a22f80a1f581a5d75f36031dfe6971882d6d7
Nguyễn Đình Duy <nguyenduy@mac.lan>
2025-03-26 00:24:46 -0500

    End of section 14

--- API/Controllers/BuggyController.cs
+++ API/Controllers/BuggyController.cs
@@ -1,9 +1,12 @@
 using System;
+using System.Security.Claims;
 using API.DTOs;
 using Core.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers;
+
 
 public class BuggyController : BaseAPIController
 {
@@ -35,5 +38,15 @@ public class BuggyController : BaseAPIController
     public IActionResult GetValidationError(CreateProductDto product)
     {
         return Ok();
+    }
+
+    [Authorize]
+    [HttpGet("secret")]
+    public IActionResult GetSecret()
+    {
+        var name = User.FindFirst(ClaimTypes.Name)?.Value;
+        var id = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+        return Ok(name + id);
     }
 }
