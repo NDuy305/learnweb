9218af65eb90bf54bd1896b906707e3d4bde70bd
Nguyễn Đình Duy <nguyenduy@mac.lan>
2025-08-06 01:06:28 -0500

    End of section 16

--- client/src/environments/environment.development.ts
+++ client/src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: "https://localhost:5001/api/"
+    apiUrl: "https://localhost:5001/api/",
+    stripePublicKey: 'pk_test_51RiQyYDCfiu1DPY5S9sC7hIueJVdJoqsog8OeWxbsUj6sTC1wvWAtqNPcMrObxK3fyZlzrhX1AqlS0SfymLWQa7f00b4zAQk39'
 };
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: "api/"
+    apiUrl: "api/",
+    stripePublicKey: 'pk_test_51RiQyYDCfiu1DPY5S9sC7hIueJVdJoqsog8OeWxbsUj6sTC1wvWAtqNPcMrObxK3fyZlzrhX1AqlS0SfymLWQa7f00b4zAQk39'
 };
